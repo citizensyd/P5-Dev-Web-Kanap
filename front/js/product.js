9ab33b8354c7654c6d0eaf082dcbc73559d3072b
citizensyd <guillard.fab@pm.me>
2022-10-25 21:45:51 +0200

    upload (product.js): add image, title, price, description, color

---
+++ front/js/product.js
@@ -0,0 +1,35 @@
+(async function() {
+    const productId = getProductId()
+    console.log(productId)
+    const product = await getProduct(productId)
+    console.log(product)
+    hydrateProduct(product)
+})()
+
+function getProductId() {
+    return new URL(location.href).searchParams.get("id")
+}
+
+function getProduct(productId) {
+    return fetch(`http://localhost:3000/api/products/${productId}`)
+    .then(function(res) {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then(function(value) {
+      return value
+    })
+    .catch(function(error) {
+      alert(error)
+    })
+  }
+
+  function hydrateProduct(product) {
+    document.getElementsByClassName('item__img')[0].innerHTML +=`<img src="${product.imageUrl}" alt="${product.altTxt}"></img>`
+    document.getElementById("title").innerHTML +=`${product.name}`
+    document.getElementById("price").innerHTML +=`${product.price}`
+    document.getElementById("colors").innerHTML +=`
+    <option value="vert">vert</option>
+    <option value="blanc">blanc</option>`
+  }
